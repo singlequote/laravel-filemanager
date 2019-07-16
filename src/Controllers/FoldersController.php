073a8fff9a99216ed4f95faaefe86937b51370a0
Wimurk <wimurk@users.noreply.github.com>
2019-07-16 14:50:43 +0200

    Update FoldersController.php

--- src/Controllers/FoldersController.php
+++ src/Controllers/FoldersController.php
@@ -29,7 +29,7 @@ class FoldersController extends \SingleQuote\FileManager\FileManager
             abort(503);
         }
         
-        return cache()->tags(['laravel-filemanager', 'laravel-filemanager:folders'])->remember('laravel-filemanager:folders-'. Str::slug($this->driver), 3600, function(){
+        return cache()->tags(['laravel-filemanager', 'laravel-filemanager:folders'])->remember("fo".md5($this->driver), 3600, function(){
             $items = File::files($this->getPath());
             $folders = [];
             foreach ($items as $item) {
